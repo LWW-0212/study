ba9de7800cc9e31ec8e6527dff5ef35aa25d0745
shanyeqianli <2058991758@qq.com>
2022-09-07 21:36:05 +0800

    几个基础作业

--- zuoye/源.cpp
+++ zuoye/源.cpp
@@ -1,7 +1,7 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-//初始化为零！！！
+///////初始化为零！！！
 //1.  void init(int* shou,int sz)
 //{
 //	int i = 0;
@@ -19,7 +19,7 @@
 //			arr[i] = 0;
 //		}
 // }
-//倒置元素
+///////倒置元素
 //  1....void reverse(int* arr, int sz)
 //{
 //	int i = 0;
@@ -46,7 +46,7 @@
 //		right--;
 //	}
 //}
-//写一个print函数
+////////写一个print函数
 //void print(int* arr, int sz)
 //{
 //	int a = 0;
@@ -92,39 +92,131 @@
 //	return 0;
 //}
 
-//统计二进制中1的个数
-int count_bit_one(int a)
-{
-	int count = 0;
-	int  i = 0;
-	for(i=0;i<32;i++)
-	{
-		if (((a>>i) & 1) == 1)
-		{
-			count++;
-		}
-    }
-	return count;
-}
+////////统计二进制中1的个数
+//int count_bit_one(int a)
+//{
+//	int count = 0;
+//	int  i = 0;
+//	for(i=0;i<32;i++)
+//	{
+//		if (((a>>i) & 1) == 1)
+//		{
+//			count++;
+//		}
+//    }
+//	return count;
+//}
+//
+//int count_bit_one(unsigned int a)
+//{
+//	int count = 0;
+//	while (a)
+//	{
+//		if (a % 2 == 1)
+//		{
+//			count++;
+//		}
+//		a = a / 2;//其实a/2就相当于a的二进制位向右移动1位
+//	}
+//	return count;
+//}
+//int count_bit_one(int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int count = count_bit_one(a);
+//	printf("%d ", count);
+//	return 0;
+//}
+////////计算两个数二进制不同的位数
+//int count_difference(int a, int b)
+//{
+//	int c = a ^ b;
+//	int count = 0;
+//	while (c)
+//	{
+//		c = c & (c - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int a = -1;
+//	int b = 3;
+//	int result = count_difference(a, b);
+//	printf("%d ", result);
+//	return 0;
+//}
 
-int count_bit_one(unsigned int a)
+//void get_ji(int a)
+//{
+//	printf("奇数位 ");
+//    for(int i=30;i>=0;i-=2)
+//	{
+//		if ((a >> (i)) & 1 == 1)
+//		{
+//			printf("1 ");
+//		}
+//		else
+//			printf("0 ");
+//	}
+//	printf("\n");
+//}
+//void get_ou(int a)
+//{
+//	printf("偶数位 ");
+//	for (int i = 31; i >=1; i -= 2)
+//	{
+//		if ((a >> (i)) & 1 == 1)
+//		{
+//			printf("1 ");
+//		}
+//		else
+//			printf("0 ");
+//	}
+//	printf("\n");
+//}
+//int main()
+//{
+//	int a = 3;
+//	 get_ou(a);
+//	 get_ji(a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 13;
+//	int b = 9;
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a=%d,b=%d", a, b);
+//	return 0;
+//}
+
+void print(int* p, int sz)
 {
-	int count = 0;
-	while (a)
+	for (int a = 0; a < sz; a++)
 	{
-		if (a % 2 == 1)
-		{
-			count++;
-		}
-		a = a / 2;//其实a/2就相当于a的二进制位向右移动1位
+		printf("%d,", *(p + a));
 	}
-	return count;
 }
 int main()
 {
-	int a = 0;
-	scanf("%d", &a);
-	int count = count_bit_one(a);
-	printf("%d ", count);
+	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(arr, sz);
 	return 0;
 }
